2cf7a8f35dd94b6dbfd6e8bd0306d3c92dfc6e36
Lakshya Sharma <lakshyasharma088@gmail.com>
2021-03-26 14:29:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoItem from './components/TodoItem/TodoItem'
+import EntryInput from './components/EntryInput/EntryInput'
+
+class App extends Component {
+  state = {
+    entries: ['React.js', 'Node.js'],
+    inputValue: ''
+  }
+
+  inputChangedHandler = (event) => {
+    // const inputValue = event.target.value;
+    // const entries = [...this.state.entries];
+    // entries.push(inputValue)
+    // this.setState({entries: entries})
+    this.setState({inputValue: event.target.value})
+
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    e.target.reset() 
+    const entry = this.state.inputValue
+    if (entry === '') return
+    const entries = [...this.state.entries];
+    entries.push(entry)
+    this.setState({ entries: entries })
+    
+  }
+
+  deleteEntryHandler = (i) => {
+    const entries = [...this.state.entries]
+    entries.splice(i, 1)
+    this.setState({entries: entries})
+  }
+
+  clear = () => {
+    const entries = []
+    this.setState({entries: entries})
+  }
+  
+  render() {
+    let entriesList = this.state.entries.map((entry, i) => {
+      return <TodoItem entry={entry} delete={() => this.deleteEntryHandler(i)} />
+    })
+
+    return (
+      <div className="App">
+        <h1>TO-DO LIST</h1>
+        <EntryInput
+          changed={this.inputChangedHandler}
+          submit={this.submitHandler}
+        />
+        {entriesList}
+        <p>{this.state.entries.length} {this.state.entries.length === 1 ? 'task': 'tasks' } left!</p>
+        <button className='clr' onClick={this.clear}>Clear All</button>
+      </div>
+    );
+  }
 }
 
 export default App;
